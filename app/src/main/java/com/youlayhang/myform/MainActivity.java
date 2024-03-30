337b213f58b20fe7305756669256ffad53ef0f6d
youlay <youlay.hong@gmail.com>
2024-03-30 13:48:12 +0700

    Form layout using LinearLayout with drawable image background

---
+++ app/src/main/java/com/youlayhang/myform/MainActivity.java
@@ -0,0 +1,49 @@
+package com.youlayhang.myform;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txnFname,txnLname;
+    Button btnSend;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        txnFname = (EditText)findViewById(R.id.txtFname);
+        txnLname = (EditText)findViewById(R.id.txtLname);
+        btnSend = (Button)findViewById(R.id.btnSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txnFname.getText().toString().equals("YOULAY")){
+                    Intent i = new Intent(MainActivity.this,Regsiterform.class);
+                    startActivity(i);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Please fill all correct inform", Toast.LENGTH_LONG).show();
+                }
+//                Toast.makeText(getApplicationContext(), "Hello Click", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/com/youlayhang/myform/Regsiterform.java
@@ -0,0 +1,14 @@
+package com.youlayhang.myform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class Regsiterform extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.regsiterform);
+    }
+}
